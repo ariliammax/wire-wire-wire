7f48aaf61d6dda48563d295a15d9e4ad1023a5d8
Max Snyder <maxsnyder2000@gmail.com>
2023-02-13 18:47:43 -0500

    setup init

--- setup.py
+++ setup.py
@@ -3,9 +3,7 @@ from setuptools import setup
 setup(name='chat',
       version='0.1',
       packages=['common',
-                'wire',
-                'wire.client',
-                'wire.server'],  # eventually also 
+                'wire'],  # eventually also 
       package_dir={'common': 'chat/common/',
                    'wire': 'chat/wire/'},
       install_requires=[])
