7b8b36110114c656aa6047710234031ca218e5b1
Max Snyder <maxsnyder2000@gmail.com>
2023-02-13 19:13:31 -0500

    Basic server/client code

--- chat/wire/server/server.py
+++ chat/wire/server/server.py
@@ -1,2 +1,16 @@
-print("server")
-input("")
+import socket
+
+HOST = "104.28.39.32"
+PORT = 65432
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((HOST, PORT))
+    s.listen()
+    conn, addr = s.accept()
+    with conn:
+        print(f"Connected by {addr}")
+        while True:
+            data = conn.recv(1024)
+            if not data:
+                break
+            conn.sendall(data)
