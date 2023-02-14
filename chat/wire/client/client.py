987a7a5c206ccba49b5b3df5ec0dbfbb6ab25ce0
Max Snyder <maxsnyder2000@gmail.com>
2023-02-13 20:07:51 -0500

    threading

--- chat/wire/client/client.py
+++ chat/wire/client/client.py
@@ -1,7 +1,7 @@
 import socket
 
-HOST = "104.28.39.32"
-PORT = 65432
+HOST = "10.250.25.88"
+PORT = 8080
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
@@ -9,4 +9,4 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     data = s.recv(1024)
     data = data.decode("utf-8")
 
-print(f"Recieved {data!r}")
+print(f"Recv-ed {data!r}")
