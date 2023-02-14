a4551f5e530922f0745dcf856765123a4e08d47f
Max Snyder <maxsnyder2000@gmail.com>
2023-02-13 20:07:51 -0500

    threading

--- chat/wire/server/server.py
+++ chat/wire/server/server.py
@@ -1,16 +1,49 @@
 import socket
 
-HOST = "104.28.39.32"
-PORT = 65432
+import threading
+
+HOST = "10.250.25.88"
+PORT = 8080
+TIMEOUT = 1
+
+
+class A:
+    def __init__(self):
+        self.a = 0
+
+    def update(self):
+        self.a += 1
+
+
+theA = A()
+
+
+def handle_connection(connection, address):
+    print(theA.a)
+    theA.update()
+    data = connection.recv(1024)
+
+    import time
+    time.sleep(2)
+    print(theA.a)
+    if not data:
+        print("TEST")
+    connection.sendall(data)
+
+
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((HOST, PORT))
     s.listen()
-    conn, addr = s.accept()
-    with conn:
-        print(f"Connected by {addr}")
-        while True:
-            data = conn.recv(1024)
-            if not data:
-                break
-            conn.sendall(data)
+    s.settimeout(TIMEOUT)
+    threads = []
+    while True:
+        try:
+            connection, address = s.accept()
+            # handle_connection(connection, address)
+            thread = threading.Thread(target=handle_connection, args=[connection, address])
+            thread.start()
+            threads.append(thread)
+        except TimeoutError:
+            pass
+    s.settimeout(None)
