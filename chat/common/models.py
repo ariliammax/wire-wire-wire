c963d483f0b184c4189f3809783a726f9e922d4c
Max Snyder <maxsnyder2000@gmail.com>
2023-02-13 22:34:14 -0500

    models

---
+++ chat/common/models.py
@@ -0,0 +1,88 @@
+from typing import Optional
+
+# DATA MODEL
+
+class Account(object):
+    def __init__(self):
+        self._logged_in: bool = False
+        self._username: str = ""
+
+class Message(object):
+    def __init__(self):
+        self._delivered: bool = False
+        self._message: str = ""
+        self._recipient_username: str = ""
+        self._sender_username: str = ""
+        self._time: int = 0
+
+# OBJECT MODEL
+
+class BaseRequest(object):
+    def __init__(self):
+        pass
+
+class BaseResponse(object):
+    def __init__(self):
+        self._error: Optional[str] = None
+
+# Function 0: Log In Account
+
+class LogInAccountRequest(BaseRequest):
+    def __init__(self):
+        self._username: str = ""
+
+class LogInAccountResponse(BaseResponse):
+    def __init__(self):
+        pass
+
+# Function 1: Create Account
+
+class CreateAccountRequest(BaseRequest):
+    def __init__(self):
+        self._username: str = ""
+
+class CreateAccountResponse(BaseResponse):
+    def __init__(self):
+        pass
+
+# Function 2: List Accounts
+
+class ListAccountsRequest(BaseRequest):
+    def __init__(self):
+        pass
+
+class ListAccountsResponse(BaseResponse):
+    def __init__(self):
+        self._accounts: list[Account] = []
+
+# Function 3: Send Message
+
+class SendMessageRequest(BaseRequest):
+    def __init__(self):
+        self._message: str = ""
+        self._recipient_username: str = ""
+        self._sender_username: str = ""
+
+class SendMessageResponse(BaseResponse):
+    def __init__(self):
+        pass
+
+# Function 4: Deliver Undelivered Messages
+
+class DeliverUndeliveredMessagesRequest(BaseRequest):
+    def __init__(self):
+        self._username: str = ""
+
+class DeliverUndeliveredMessagesResponse(BaseResponse):
+    def __init__(self):
+        self._messages: list[Message] = []
+
+# Function 5: Delete Account
+
+class DeleteAccountRequest(BaseRequest):
+    def __init__(self):
+        self._username: str = ""
+
+class DeleteAccountResponse(BaseResponse):
+    def __init__(self):
+        pass
--- chat/wire/client/client.py
+++ chat/wire/client/client.py
@@ -1,10 +1,9 @@
+from chat.common.config import Config
+
 import socket
 
-HOST = "10.250.25.88"
-PORT = 8080
-
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST, PORT))
+    s.connect((Config.HOST, Config.PORT))
     s.sendall(b"Hello world")
     data = s.recv(1024)
     data = data.decode("utf-8")
