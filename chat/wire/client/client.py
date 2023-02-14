3dc4d14c0f08a7dce782e4542f7d794034853db1
Max Snyder <maxsnyder2000@gmail.com>
2023-02-13 19:17:01 -0500

    client

--- chat/wire/client/client.py
+++ chat/wire/client/client.py
@@ -1,2 +1,12 @@
-print("client")
-input("")
+import socket
+
+HOST = "104.28.39.32"
+PORT = 65432
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect((HOST, PORT))
+    s.sendall(b"Hello world")
+    data = s.recv(1024)
+    data = data.decode("utf-8")
+
+print(f"Recieved {data!r}")
