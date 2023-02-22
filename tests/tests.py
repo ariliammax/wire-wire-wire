42287a5d76d64c3d030290397da1b1eaa1a0b9d9
William McInroy <mcinroy@college.harvard.edu>
2023-02-22 09:10:49 -0500

    refactor tests + add login

--- tests/tests.py
+++ tests/tests.py
@@ -23,20 +23,20 @@ class Chat(Enum):
     WIRE = 1
 
 
-def start_client(chat: Chat):
+def start_client(chat: Chat, host='localhost', port=Config.PORT):
     match chat:
         case Chat.GRPC:
-            return grpc_client_entry(Config.HOST, Config.PORT)
+            return grpc_client_entry(host=host, port=port)
         case Chat.WIRE:
-            return wire_client_entry(Config.HOST, Config.PORT + 1)
+            return wire_client_entry(host=host, port=port + 1)
 
 
-def start_server(chat: Chat):
+def start_server(chat: Chat, host='localhost', port=Config.PORT):
     match chat:
         case Chat.GRPC:
-            grpc_server_main(Config.PORT)
+            grpc_server_main(port=port)
         case Chat.WIRE:
-            wire_server_main(Config.HOST, Config.PORT + 1)
+            wire_server_main(host=host, port=port + 1)
 
 
 def start_server_thread(chat: Chat):
@@ -53,31 +53,87 @@ def request(chat: Chat, opcode: Opcode, **kwargs):
             return wire_client_request(opcode, **kwargs)
 
 
+class Tests:
+
+    @staticmethod
+    def clean_between_tests():
+        Database.delete_all()
+
+    @staticmethod
+    def run_create_account_success(chat: Chat, **kwargs):
+        # Test 1 - create account works
+        response = request(chat,
+                           Opcode.CREATE_ACCOUNT,
+                           username="username",
+                           **kwargs)
+        assert (response.get_error() == '')
+
+    @staticmethod
+    def run_create_account_fail(chat: Chat, **kwargs):
+        # Test 2 - create already created account fails
+        Tests.run_create_account_success(chat, **kwargs)
+        response = request(chat,
+                           Opcode.CREATE_ACCOUNT,
+                           username="username",
+                           **kwargs)
+        assert (response.get_error() != '')
+
+    @staticmethod
+    def run_log_in_account_success(chat: Chat, **kwargs):
+        # Test 3 - log in account works
+        Tests.run_create_account_success(chat, **kwargs)
+        response = request(chat,
+                           Opcode.LOG_IN_ACCOUNT,
+                           username="username",
+                           **kwargs)
+        assert (response.get_error() == '')
+
+    @staticmethod
+    def run_log_in_account_fail(chat: Chat, **kwargs):
+        # Test 4 - log in nonexistant account fails
+        response = request(chat,
+                           Opcode.LOG_IN_ACCOUNT,
+                           username="username",
+                           **kwargs)
+        assert (response.get_error() != '')
+
+    @staticmethod
+    def run_list_account_success(chat: Chat, **kwargs):
+        # Test 3 - list accounts works
+        Tests.run_log_in_account_success(chat, **kwargs)
+        response = request(chat,
+                           Opcode.LIST_ACCOUNTS,
+                           text_wildcard="username",
+                           **kwargs)
+        assert (len(response.get_accounts()) == 1)
+
+    @staticmethod
+    def run_list_account_fail(chat: Chat, **kwargs):
+        # Test 4 - list account "fails"
+        Tests.run_log_in_account_success(chat, **kwargs)
+        response = request(chat,
+                           Opcode.LIST_ACCOUNTS,
+                           text_wildcard="username2",
+                           **kwargs)
+        assert (len(response.get_accounts()) == 0)
+
+
 def run_tests(chat: Chat):
     start_server_thread(chat)
     sleep(0.1)
     kwargs = start_client(chat)
 
-    # Test 1
-    response = request(chat,
-                       Opcode.CREATE_ACCOUNT,
-                       username="username",
-                       **kwargs)
-    assert (response.get_error() == '')
-    Database.delete_all()
+    Tests.run_create_account_success(chat, **kwargs)
+    Tests.clean_between_tests()
+    Tests.run_create_account_fail(chat, **kwargs)
+    Tests.clean_between_tests()
 
-    # Test 2
-    response = request(chat,
-                       Opcode.CREATE_ACCOUNT,
-                       username="username",
-                       **kwargs)
-    assert (response.get_error() == '')
-    response = request(chat,
-                       Opcode.CREATE_ACCOUNT,
-                       username="username",
-                       **kwargs)
-    assert (response.get_error() != '')
-    Database.delete_all()
+    Tests.run_log_in_account_success(chat, **kwargs)
+    Tests.clean_between_tests()
+    Tests.run_log_in_account_fail(chat, **kwargs)
+    Tests.clean_between_tests()
+
+    return
 
 
 def test_wire():
